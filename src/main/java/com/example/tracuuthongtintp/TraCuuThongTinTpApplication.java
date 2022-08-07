232232bffd3ec2ddf5bb641fd713ac2b15c8c159
hung.vm.dev <hungvumanh2001@gmail.com>
2022-08-07 10:35:19 +0700

    first commit

---
+++ src/main/java/com/example/tracuuthongtintp/TraCuuThongTinTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracuuthongtintp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraCuuThongTinTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraCuuThongTinTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tracuuthongtintp/TraCuuThongTinTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tracuuthongtintp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraCuuThongTinTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
